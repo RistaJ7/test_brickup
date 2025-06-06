789434970ca00591026e76c5a5ae9641359e10a5
Lucas Ferreira Nogueira <lucas.ferreiranogueira@outlook.com>
2025-06-06 13:53:19 -0300

    :recycle: REFACTOR: correção do header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,19 @@
-import { Layout } from "antd";
+import { Flex, Layout, Space } from "antd";
+import { Typography } from 'antd';
+import Title from "antd/es/typography/Title";
 
 const { Header } = Layout;
+const { Text } = Typography;
 
 const GlobalHeader = () => {
     return (
         <Header style={{ padding: 0, background: "#fff" }}>
-
+            <Flex justify="center">
+                <Space align="baseline" justify={'center'}>
+                    <Title style={{ margin: 0 }}>Construction</Title>
+                    <Text type="warning" strong>Build and innovation</Text>
+                </Space>
+            </Flex>
         </Header>
     );
 };
